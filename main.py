b388691cea6038740eaf62fbc8635ce76cbba1ef
ivancadre <75526722+ivancadre@users.noreply.github.com>
2021-02-27 15:07:52 +0100

    Update main.py

--- main.py
+++ main.py
@@ -7,18 +7,18 @@ goalscorer_one = 'Gullit'
 goalscorer_two = 'Van Basten'
 goal_0 = 32
 goal_1 = 54
-scorers = f'{goalscorer_one} {str(goal_0)}, {goalscorer_two} {str(goal_1)}'
+scorers = goalscorer_one + " " + str(goal_0), goalscorer_two + " " + str(goal_1)
 report = f'{goalscorer_one} scored in the {goal_0}th minute,\n{goalscorer_one} scored in the {goal_1}th minute'
 print(report)
 player = 'Dimovski Protasov'
 first_name = player[:player.find(' ')]
 print(first_name)
-last_name_len = player[(player.find(' ')+1):len(player)]
+last_name_len = len(player[(player.find(' ')+1):])
 print(last_name_len)
-name_short = f'{first_name[:1]}. {last_name_len}'
+name_short = f'{first_name[0]}. {player[9:]}'
 print(name_short)
 chant = f'{first_name}! '*len(first_name)
 chant = chant[:-1]
 print(chant)
-good_chant = chant != ' '
+good_chant = chant[-1] != ' '
 print(good_chant)
